5e67bafbd2a7b499fbd856366ba3bd5087331d03
Kim Zachariassen <kza@z4it.com>
2023-11-27 09:01:28 +0100

    NatGatewayAdded

---
+++ modules/common/azurerm_nat_gateway/v1.0.0/outputs.tf
@@ -0,0 +1,4 @@
+output "id" {
+  value       = azurerm_nat_gateway.nat_gateway.id
+  description = "The nat gateway ID."
+}
--- modules/custom/containerinfrastructure/v1.0.0/main.tf
+++ modules/custom/containerinfrastructure/v1.0.0/main.tf
@@ -44,9 +44,9 @@ module "subnet_containerapps" {
   depends_on = [ module.subnet_applicationgateway ]
 }
 
-module "public_ip" {
+module "public_ip_application_gateway" {
   source = "../../../common/azurerm_public_ip/v1.0.0"
-  public_ip_name = "pip-${random_string.deployment_string.result}"
+  public_ip_name = "pip-agw-${random_string.deployment_string.result}"
   resource_group_name = module.resource_group.name
   location = var.location
   common_tags = var.common_tags
@@ -62,7 +62,7 @@ module "application_gateway" {
   autoscale_configuration   = { min_capacity = 1, max_capacity = 2 }
   waf_configuration         = { enabled = true, firewall_mode = "Detection", rule_set_version = "3.0"}
   subnet_id                 = module.subnet_applicationgateway.id
-  frontend_ip_configuration = { public_ip_address_id = module.public_ip.id, private_ip_address = "10.0.0.10", private_ip_address_allocation = "Static" }
+  frontend_ip_configuration = { public_ip_address_id = module.public_ip_application_gateway.id, private_ip_address = "10.0.0.10", private_ip_address_allocation = "Static" }
   backend_address_pools = [
     { name = "backend-address-pool-1", ip_addresses = "10.0.0.4,10.0.0.5,10.0.0.6" }
   ]
@@ -77,4 +77,30 @@ module "application_gateway" {
       backend_http_settings_name = "backend-http-setting"
     }
   ]
+}
+
+module "public_ip_nat_gateway" {
+  source = "../../../common/azurerm_public_ip/v1.0.0"
+  public_ip_name = "pip-natg-${random_string.deployment_string.result}"
+  resource_group_name = module.resource_group.name
+  location = var.location
+  common_tags = var.common_tags
+}
+
+module "nat_gateway" {
+  source = "../../../common/azurerm_nat_gateway/v1.0.0"
+  nat_gateway_name = "natg-${random_string.deployment_string.result}"
+  resource_group_name = module.resource_group.name
+  location = var.location
+  common_tags = var.common_tags
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "nat_gateway" {
+  nat_gateway_id       = module.nat_gateway.id
+  public_ip_address_id = module.public_ip_nat_gateway.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "nat_gateway_subnet_containerapps" {
+  subnet_id      = module.subnet_containerapps.id
+  nat_gateway_id = module.nat_gateway.id
 }
