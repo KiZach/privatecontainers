c09638f2d6058bf1708b6fb21e94e7b835701e0e
Kim Zachariassen <kza@z4it.com>
2023-12-05 11:14:43 +0100

    OutputAdded

--- modules/common/azurerm_public_ip/v1.0.0/outputs.tf
+++ modules/common/azurerm_public_ip/v1.0.0/outputs.tf
@@ -2,3 +2,7 @@ output "id" {
   value       = azurerm_public_ip.public_ip.id
   description = "The public IP ID."
 }
+output "ip_address" {
+  value       = azurerm_public_ip.public_ip.ip_address
+  description = "The public IP address."
+}
