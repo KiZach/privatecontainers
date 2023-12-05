db52a692f950201fb19bb178594aa7d86d4c0b90
Kim Zachariassen <kza@z4it.com>
2023-12-05 11:14:28 +0100

    NatGatewayRemoved

--- modules/common/azurerm_nat_gateway/v1.0.0/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_nat_gateway" "nat_gateway" {
-  name                = var.nat_gateway_name
-  sku_name            = var.nat_gateway_sku
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  tags                = var.common_tags
-}
