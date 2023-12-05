d428f4cf2625b78af7a06608c60311cd89bd5c90
Kim Zachariassen <kza@z4it.com>
2023-12-05 11:15:02 +0100

    OutputAdded

--- modules/common/azurerm_log_analytics_workspace/v1.0.0/outputs.tf
+++ modules/common/azurerm_log_analytics_workspace/v1.0.0/outputs.tf
@@ -2,3 +2,11 @@ output "id" {
   value       = azurerm_log_analytics_workspace.log_analytics_workspace.id
   description = "The log analytics ID."
 }
+output "workspace_id" {
+  value       = azurerm_log_analytics_workspace.log_analytics_workspace.workspace_id
+  description = "The log analytics ID."
+}
+output "primary_shared_key" {
+  value       = azurerm_log_analytics_workspace.log_analytics_workspace.primary_shared_key
+  description = "The log analytics ID."
+}
